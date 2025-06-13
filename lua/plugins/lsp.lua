4f41e629820e8c697df948efe9e1ed3213b60a23
Leo Martins <loemra.dev@gmail.com>
2025-06-13 11:43:44 -0700

    update lsp to nvim11 compliant

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -40,7 +40,8 @@ return {
 
 			for server, config in pairs(opts.servers) do
 				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				require("lspconfig")[server].setup(config)
+				vim.lsp.enable(server);
+				vim.lsp.config(server, config);
 			end
 		end,
 	},
